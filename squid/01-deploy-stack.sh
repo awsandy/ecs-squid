9154f381025d2a001855395c56a47fa6c5938760
awsandy <awsandy@amazon.co.uk>
2023-06-28 15:38:37 +0100

    vf update

--- squid/01-deploy-stack.sh
+++ squid/01-deploy-stack.sh
@@ -1,4 +1,4 @@
 date
-aws cloudformation deploy --stack-name squid-ecr --template-file cloudformation/centralised_egress_proxy.yaml \
+aws cloudformation deploy --stack-name squid-ecr --template-file ../squid-docker/centralised_egress_proxy.yaml \
 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" 
 date
