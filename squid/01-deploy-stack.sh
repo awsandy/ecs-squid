995109bd53bedf9dcc9f36b45eb755e615416e95
awsandy <awsandy@amazon.co.uk>
2023-06-28 14:11:31 +0100

    up

--- squid/01-deploy-stack.sh
+++ squid/01-deploy-stack.sh
@@ -1,4 +1,4 @@
 date
-aws cloudformation deploy --stack-name squid-ecr --template-file cloudformation/centralised_egress_proxy.yml \
+aws cloudformation deploy --stack-name squid-ecr --template-file cloudformation/centralised_egress_proxy.yaml \
 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
 date
