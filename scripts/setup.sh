7dd1e3a8b272c0891b18be1c9fa2a1457f651f0e
awsandy <awsandy@amazon.co.uk>
2023-07-05 20:54:19 +0100

    up

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,19 @@
 echo "Install OS tools"
-sudo yum -y -q -e 0 install nodejs siege jq moreutils nmap > /dev/null
+sudo yum -y -q -e 0 install nodejs siege jq moreutils nmap >/dev/null
 echo "Update OS tools"
-sudo yum update -y > /dev/null
+sudo yum update -y >/dev/null
 echo "Update pip"
-sudo pip install --upgrade pip 2&> /dev/null
+sudo pip install --upgrade pip 2 &>/dev/null
 
 #
 echo "Uninstall AWS CLI v1"
-sudo /usr/local/bin/pip uninstall awscli -y 2&> /dev/null
-sudo pip uninstall awscli -y 2&> /dev/null
+sudo /usr/local/bin/pip uninstall awscli -y 2 &>/dev/null
+sudo pip uninstall awscli -y 2 &>/dev/null
 echo "Install AWS CLI v2"
-curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null
+curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" >/dev/null
 unzip -qq awscliv2.zip
-sudo ./aws/install > /dev/null
-echo "alias aws='/usr/local/bin/aws'" >> ~/.bash_profile
+sudo ./aws/install >/dev/null
+echo "alias aws='/usr/local/bin/aws'" >>~/.bash_profile
 source ~/.bash_profile
 rm -f awscliv2.zip
 rm -rf aws
@@ -32,7 +32,7 @@ echo "export TF_VAR_region=${AWS_REGION}" | tee -a ~/.bash_profile
 aws configure set default.region ${AWS_REGION}
 aws configure get region
 
-aws sts get-caller-identity --query Arn | grep AWSCloud9SSMAccessRole > /dev/null
+aws sts get-caller-identity --query Arn | grep AWSCloud9SSMAccessRole >/dev/null
 if [ $? -eq 0 ]; then
   rm -vf ${HOME}/.aws/credentials
 else
@@ -41,12 +41,10 @@ else
 fi
 
 echo "Setup Terraform cache"
-if [ ! -f $HOME/.terraform.d/plugin-cache ];then
+if [ ! -f $HOME/.terraform.d/plugin-cache ]; then
   mkdir -p $HOME/.terraform.d/plugin-cache
   cp tf-setup/dot-terraform.rc $HOME/.terraformrc
 fi
-
-
 
 echo "ssh key"
 if [ ! -f ~/.ssh/id_rsa ]; then
@@ -57,19 +55,18 @@ fi
 #
 echo "ssm cli add on"
 curl --silent "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
-sudo yum install -y session-manager-plugin.rpm > /dev/null
+sudo yum install -y session-manager-plugin.rpm >/dev/null
 rm -f ~/environment/session-manager-plugin.rpm
 #
 
 # cleanup key_pair if already there
-aws ec2 delete-key-pair --key-name "eksworkshop" > /dev/null
-
+aws ec2 delete-key-pair --key-name "eksworkshop" >/dev/null
 
 echo "pip3"
 curl --silent "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python3 get-pip.py 2&> /dev/null
+python3 get-pip.py 2 &>/dev/null
 echo "git-remote-codecommit"
-pip3 install git-remote-codecommit 2&> /dev/null
+pip3 install git-remote-codecommit 2 &>/dev/null
 
 # ------  resize OS disk -----------
 # Specify the desired volume size in GiB as a command-line argument. If not specified, default to 32 GiB.
@@ -85,59 +82,56 @@ VOLUME_ID=$(aws ec2 describe-instances \
   --output text)
 
 # Resize the EBS volume.
-aws ec2 modify-volume --volume-id $VOLUME_ID --size $VOLUME_SIZE > /dev/null
+aws ec2 modify-volume --volume-id $VOLUME_ID --size $VOLUME_SIZE >/dev/null
 
 # Wait for the resize to finish.
 while [ \
   "$(aws ec2 describe-volumes-modifications \
     --volume-id $VOLUME_ID \
     --filters Name=modification-state,Values="optimizing","completed" \
-    --query "length(VolumesModifications)"\
+    --query "length(VolumesModifications)" \
     --output text)" != "1" ]; do
-sleep 1
+  sleep 1
 done
 
-if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
-then
+if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]; then
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
- 
+
   # Expand the size of the file system.
-  sudo resize2fs /dev/xvda1 > /dev/null
+  sudo resize2fs /dev/xvda1 >/dev/null
 
 else
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/nvme0n1 1
 
   # Expand the size of the file system.
-  sudo resize2fs /dev/nvme0n1p1 &> /dev/null #(Amazon Linux 1)
-  sudo xfs_growfs /dev/nvme0n1p1 &> /dev/null #(Amazon Linux 2)
+  sudo resize2fs /dev/nvme0n1p1 &>/dev/null  #(Amazon Linux 1)
+  sudo xfs_growfs /dev/nvme0n1p1 &>/dev/null #(Amazon Linux 2)
 fi
 df -m /
 #
 #
 
 echo "Verify ...."
-for command in jq aws wget terraform
-  do
-    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
-  done
+for command in jq aws wget terraform; do
+  which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+done
 
-
-this=`pwd`
+this=$(pwd)
 #echo "sample apps"
 cd ~/environment
 
 echo "Enable bash_completion"
-. /etc/profile.d/bash_completion.sh &> /dev/null
+. /etc/profile.d/bash_completion.sh &>/dev/null
 . ~/.bash_completion
-echo "alias tfb='terraform init && terraform plan -out tfplan && terraform apply tfplan'" >> ~/.bash_profile
-echo "alias aws='/usr/local/bin/aws'" >> ~/.bash_profile
+echo "alias tfb='terraform init && terraform plan -out tfplan && terraform apply tfplan'" >>~/.bash_profile
+echo "alias aws='/usr/local/bin/aws'" >>~/.bash_profile
 source ~/.bash_profile
 #
-echo "export AWS_DEFAULT_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)" >> ~/.bashrc
-echo "export AWS_REGION=\$AWS_DEFAULT_REGION" >> ~/.bashrc
-echo "export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)" >> ~/.bashrc
+echo "export AWS_DEFAULT_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)" >>~/.bashrc
+echo "export AWS_REGION=\$AWS_DEFAULT_REGION" >>~/.bashrc
+echo "export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)" >>~/.bashrc
 source ~/.bashrc
 test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
 
@@ -146,29 +140,26 @@ echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
 
-
-
-
 test -n "$AWS_REGION" && echo "PASSED: AWS_REGION is $AWS_REGION" || echo AWS_REGION is not set !!
 test -n "$TF_VAR_region" && echo "PASSED: TF_VAR_region is $TF_VAR_region" || echo TF_VAR_region is not set !!
 test -n "$ACCOUNT_ID" && echo "PASSED: ACCOUNT_ID is $ACCOUNT_ID" || echo ACCOUNT_ID is not set !!
-echo "setup tools run" >> ~/setup-tools.log
+echo "setup tools run" >>~/setup-tools.log
 
-profile_name="eksworkshop-admin"
+#profile_name="eksworkshop-admin"
 instance_id=$(curl -sS http://169.254.169.254/latest/meta-data/instance-id)
 ipa=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[].Instances[].IamInstanceProfile | jq -r .[].Arn)
 iip=$(aws ec2 describe-iam-instance-profile-associations --filters "Name=instance-id,Values=$instance_id" --query IamInstanceProfileAssociations[].AssociationId | jq -r .[])
-echo "Associate $profile_name"
-if aws ec2 replace-iam-instance-profile-association --iam-instance-profile "Name=$profile_name" --association-id $iip; then
-  if aws cloud9 update-environment --environment-id $C9_PID --managed-credentials-action DISABLE 2> /dev/null; then
-    rm -vf ${HOME}/.aws/credentials
-    echo "Profile eksworkshop-admin associated successfully."
-  fi
-else
-  echo "ERROR: Encountered error associating instance profile eksworkshop-admin with Cloud9 environment"
+#echo "Associate $profile_name"
+#if aws ec2 replace-iam-instance-profile-association --iam-instance-profile "Name=$profile_name" --association-id $iip; then
+if aws cloud9 update-environment --environment-id $C9_PID --managed-credentials-action DISABLE 2>/dev/null; then
+  rm -vf ${HOME}/.aws/credentials
+  echo "Disabled temporary credentials successfully."
 fi
+#else
+#  echo "ERROR: Encountered error associating instance profile eksworkshop-admin with Cloud9 environment"
+#fi
 
-## CDK 
+## CDK
 
 # Install prerequisite packages
 sudo yum -y install jq nodejs siege
@@ -177,20 +168,20 @@ sudo yum -y install jq nodejs siege
 pip3 install --user --upgrade awslogs
 
 #  Verify environment variables required to communicate with AWS API's via the cli tools
-grep AWS_DEFAULT_REGION ~/.bashrc 
+grep AWS_DEFAULT_REGION ~/.bashrc
 if [ $? -ne 0 ]; then
-    echo "export AWS_DEFAULT_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)" >> ~/.bashrc
+  echo "export AWS_DEFAULT_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)" >>~/.bashrc
 fi
 
-grep AWS_REGION ~/.bashrc 
+grep AWS_REGION ~/.bashrc
 if [ $? -ne 0 ]; then
-    echo "export AWS_REGION=\$AWS_DEFAULT_REGION" >> ~/.bashrc
+  echo "export AWS_REGION=\$AWS_DEFAULT_REGION" >>~/.bashrc
 fi
 
-grep AWS_ACCOUNT_ID ~/.bashrc 
+grep AWS_ACCOUNT_ID ~/.bashrc
 if [ $? -ne 0 ]; then
-    echo "export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)" >> ~/.bashrc
-fi 
+  echo "export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)" >>~/.bashrc
+fi
 
 source ~/.bashrc
 ## IAM roles
