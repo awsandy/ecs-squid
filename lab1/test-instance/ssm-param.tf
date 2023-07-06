57391d9abfdd0faadac9123737f396acce264c5a
awsandy <awsandy@amazon.co.uk>
2023-07-06 18:14:00 +0100

    up

--- lab1/test-instance/ssm-param.tf
+++ lab1/test-instance/ssm-param.tf
@@ -5,3 +5,19 @@ data "aws_ssm_parameter" "squid-dns" {
 data "aws_ssm_parameter" "squid-endpointservice" {
   name        = "/ecsworkshop/squid-endpointservice"
 }
+
+
+
+resource "aws_ssm_parameter" "proxy-dns" {
+  name        = "/ecsworkshop/proxy-dns"
+  description = "The private endpoint dns"
+  type        = "String"
+  value = aws_vpc_endpoint.squid_service.dns_entry[0].dns_name
+
+  tags = {
+    workshop = "ecs-workshop"
+  }
+}
+
+
+
