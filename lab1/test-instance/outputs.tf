5809542cab01b3822511538b43fcd759f78f1743
awsandy <awsandy@amazon.co.uk>
2023-07-06 16:50:10 +0100

    up

--- lab1/test-instance/outputs.tf
+++ lab1/test-instance/outputs.tf
@@ -8,6 +8,11 @@ output "ipv6" {
 output "instanceid" {
     value = "${aws_instance.ec2-priv-1.*.id}"
 }
+
+
+output "squid-endpoint" {
+  value = tolist(aws_vpc_endpoint_service.vpce-svc-09041d05a3a2059cc.base_endpoint_dns_names)[0]
+}
 #output "activation-id" {
 #    value = "${aws_ssm_activation.foo.*.id}"
 #}
