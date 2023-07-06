88150bb49f9490f5ef39134c6218dd77f82a03d7
awsandy <awsandy@amazon.co.uk>
2023-07-06 17:28:39 +0100

    up

--- lab1/test-instance/vpc-endpoint.tf
+++ lab1/test-instance/vpc-endpoint.tf
@@ -3,9 +3,9 @@ resource "aws_vpc_endpoint" "squid_service" {
   service_name      = data.aws_vpc_endpoint_service.squid.service_name
   vpc_endpoint_type = "Interface"
 
-  #security_group_ids = [
-  #  aws_security_group.ptfe_service.id,
-  #]
+  security_group_ids = [
+    aws_security_group.ingress-at.id,
+  ]
 
   subnet_ids          = [aws_subnet.subnet-priv1.id]
   private_dns_enabled = false
