71cb97a36869feff51d15c6434fc47698eacd697
awsandy <awsandy@amazon.co.uk>
2023-07-06 13:21:56 +0100

    up

--- scripts/create-role.sh
+++ scripts/create-role.sh
@@ -1,3 +1,4 @@
+aws cloud9 update-environment --environment-id $C9_PID --managed-credentials-action DISABLE
 profile_name="ecsworkshop-admin"
 aws iam create-role --role-name $profile_name --assume-role-policy-document file://trust-policy.json 
 aws iam attach-role-policy --role-name $profile_name --policy-arn arn:aws:iam::aws:policy/AdministratorAccess 
