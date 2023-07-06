42d4756779046186e0c9b1db8bf74a758f03f269
awsandy <awsandy@amazon.co.uk>
2023-07-06 16:26:04 +0100

    up

---
+++ lab1/test-instance/eip.tf
@@ -0,0 +1,8 @@
+resource "aws_eip" "eip-natgw" {
+  #instance = "${aws_instance.ec2-priv-1.id}"
+  vpc      = true
+  tags = {
+    Name = "eip-natgw"
+  }
+
+}
--- lab1/test-instance/nat-gw.tf
+++ lab1/test-instance/nat-gw.tf
@@ -2,5 +2,5 @@ resource "aws_nat_gateway" "nat-gw" {
   //other arguments
   allocation_id = "${aws_eip.eip-natgw.id}"
   subnet_id     = "${aws_subnet.subnet-pub1.id}"
-  depends_on = ["aws_internet_gateway.ec2-pub-igw"]
+  depends_on = [aws_internet_gateway.ec2-pub-igw]
 }
