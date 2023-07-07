fd729d92d46c79e96fa67dce97e0fbf182e35563
awsandy <awsandy@amazon.co.uk>
2023-07-07 12:14:02 +0100

    up

--- lab3/tf-example/dockerless-push.tf
+++ lab3/tf-example/dockerless-push.tf
@@ -21,10 +21,13 @@ resource "null_resource" "load_ecr" {
     when        = create
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-
-
-
-        echo "************************************************************************************"
+        an=$(echo ${var.app_name})
+        repo=$(echo ${aws_ecr_repository.this.name})
+        reg=$(echo ${data.aws_region.current.name})
+        acc=$(echo ${data.aws_caller_identity.current.account_id})
+        aws ecr get-login-password --region $reg | docker login --username AWS --password-stdin ${acc}.dkr.ecr.${reg}.amazonaws.com
+        docker tag ${an}:latest ${acc}.dkr.ecr.${reg}.amazonaws.com/${an}
+        docker push ${acc}.dkr.ecr.${reg}.amazonaws.com/${an}
      EOT
   }
 }
