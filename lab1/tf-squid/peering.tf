ecaf1a23dc6619a3ef385294d108595e99c864b7
awsandy <awsandy@amazon.co.uk>
2023-07-08 16:23:36 +0100

    up

--- lab1/tf-squid/peering.tf
+++ lab1/tf-squid/peering.tf
@@ -23,14 +23,14 @@ output "peerid" {
 # default to squid
 resource "aws_route" "rt-def" {
   route_table_id            = data.aws_vpc.vpc-default.main_route_table_id
-  destination_cidr_block    = aws_vpc.vpc-090fcf5a7a3b94d20.eks-cidr
+  destination_cidr_block    = aws_vpc.vpc-090fcf5a7a3b94d20.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
 }
 
 #  squid to default
 resource "aws_route" "rt-squid" {
   route_table_id            = aws_route_table.rtb-00c2a3920ce34d43f.id
-  destination_cidr_block    = data.aws_vpc.vpc-default.eks-cidr
+  destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
 }
 
