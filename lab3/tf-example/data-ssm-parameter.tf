2dd89bb13af47fe789c3ed80e8869d4c76b05c59
awsandy <awsandy@amazon.co.uk>
2023-07-08 18:35:15 +0100

    up

--- lab3/tf-example/data-ssm-parameter.tf
+++ lab3/tf-example/data-ssm-parameter.tf
@@ -25,3 +25,35 @@ data "aws_ssm_parameter" "mesh-sg" {
 data  "aws_ssm_parameter" "mesh-cluster" {
   name        = "/ecsworkshop/mesh-cluster"
 }
+
+data "aws_ssm_parameter" "squid-execution-role" {
+  name        = "/ecsworkshop/squid-execution-role"
+}
+
+data "aws_ssm_parameter" "squid-loggroup" {
+  name        = "/ecsworkshop/squid-loggroup"
+}
+
+data "aws_ssm_parameter" "squid-subnet1" {
+  name        = "/ecsworkshop/squid-subnet1"
+}
+
+data "aws_ssm_parameter" "squid-subnet2" {
+  name        = "/ecsworkshop/squid-subnet2"
+}
+
+data "aws_ssm_parameter" "squid-sg" {
+  name        = "/ecsworkshop/squid-sg"
+}
+
+data "aws_ssm_parameter" "squid-tg" {
+  name        = "/ecsworkshop/squid-tg"
+}
+
+data "aws_ssm_parameter" "squid-vpc" {
+  name        = "/ecsworkshop/squid-vpc"
+}
+
+data "aws_ssm_parameter" "squid-lbarn" {
+  name        = "/ecsworkshop/squid-lbarn"
+}
