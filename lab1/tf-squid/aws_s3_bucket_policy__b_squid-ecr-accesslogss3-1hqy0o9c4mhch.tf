2e4830ca0e835f1c0c2ab953153a7b439f3bec9e
awsandy <awsandy@amazon.co.uk>
2023-07-09 16:45:46 +0100

    up

--- lab1/tf-squid/aws_s3_bucket_policy__b_squid-ecr-accesslogss3-1hqy0o9c4mhch.tf
+++ lab1/tf-squid/aws_s3_bucket_policy__b_squid-ecr-accesslogss3-1hqy0o9c4mhch.tf
@@ -37,6 +37,16 @@ resource "aws_s3_bucket_policy" "b_squid-ecr-accesslogss3-1hqy0o9c4mhch" {
           # Resource = "arn:aws:s3:::squid-ecr-accesslogss3-1hqy0o9c4mhch/nlb/*"
           Resource = format("%s/nlb/*",aws_s3_bucket.b_squid-ecr-accesslogss3-1hqy0o9c4mhch.arn)
           Sid      = "AWSLogDeliveryWrite"
+        },
+        {
+          Action = "s3:GetBucketAcl"
+          Effect = "Allow"
+          Principal = {
+            Service = "delivery.logs.amazonaws.com"
+          }
+          #Resource = "arn:aws:s3:::squid-ecr-accesslogss3-1hqy0o9c4mhch"
+          Resource = format("%s",aws_s3_bucket.b_squid-ecr-accesslogss3-1hqy0o9c4mhch.arn)
+          Sid      = "AWSLogDeliveryAclCheck"
         }
       ]
       Version = "2012-10-17"
